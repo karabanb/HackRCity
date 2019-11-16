51a8901bdf017a361b03fdd6d2fa61f7f8e77b0c
karabanb <bkarabands@gmail.com>
2019-11-16 15:50:54 +0100

    w chuj wzbogacone

--- scripts/100_DataSummaryBK.R
+++ scripts/100_DataSummaryBK.R
@@ -7,10 +7,16 @@ load('data/things_to_do.Rdata')
 
 cleaned_data <- full_data %>%
   mutate(year = as.integer(year)) %>%
-  filter(!is.na(TURYSTYCZNE_OBIEKTY.turysci_zagraniczni_ogolem))
+  filter(!is.na(TURYSTYCZNE_OBIEKTY.turysci_zagraniczni_ogolem)) 
+
+gus_data <- GUS_additional_data %>%
+  group_by(city) %>%
+  summarise_all(list(mean), na.rm = T)
 
 
 df_summary <- df_status(cleaned_data)# variables with one unique value aren't important that's why we drop those variables
+df_summary_gus <- df_status(gus_data)
+
 
 vars_to_drop <- df_summary %>% 
   filter(unique ==  1) %>%
@@ -23,7 +29,11 @@ wiki <- full_data %>%
 
 cleaned_data <- cleaned_data %>% 
   select(-vars_to_drop, -starts_with('wiki')) %>%
-  left_join(.,wiki, by = 'city')
+  left_join(.,wiki, by = 'city') %>%
+  left_join(., gus_data, by = 'city') %>%
+  left_join(., hotels, by = 'city') %>%
+  left_join(., restaurants, by =  'city') %>%
+  left_join(., things_to_do, by = 'city')
 
 save(cleaned_data, file = 'data/100_CleanedData.Rdata')
 
