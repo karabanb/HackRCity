340b7b0c1342187196db03c29adf1b3dd51a5999
karabanb <bkarabands@gmail.com>
2019-11-16 13:15:13 +0100

    cleaned data was added

--- scripts/100_DataSummaryBK.R
+++ scripts/100_DataSummaryBK.R
@@ -4,28 +4,23 @@ load('data/full_data.Rdata')
 
 cleaned_data <- full_data %>%
   mutate(year = as.integer(year)) %>%
-  filter(year > 2010)
+  filter(!is.na(TURYSTYCZNE_OBIEKTY.turysci_zagraniczni_ogolem))
 
 
-df_summary <- df_status(select(cleaned_data, - vars_to_drop))# variables with one unique value aren't important that's why we drop those variables
+df_summary <- df_status(cleaned_data)# variables with one unique value aren't important that's why we drop those variables
 
 vars_to_drop <- df_summary %>% 
-  filter(unique == 1) %>%
+  filter(unique ==  1) %>%
   pull(variable)
 
+wiki <- full_data %>%
+  select(city, starts_with('wiki')) %>%
+  distinct() %>%
+  filter(!is.na(wikipedia_area))
 
-df_cor <- full_data %>%
-  filter(year == 2018, !is.na(touristic_popularity)) %>%
-  select(touristic_popularity, TURYSTYCZNE_OBIEKTY.turysci_zagraniczni_ogolem)
+cleaned_data <- cleaned_data %>% 
+  select(-vars_to_drop, -starts_with('wiki')) %>%
+  left_join(.,wiki, by = 'city')
 
-m1 <- lm(touristic_popularity ~ TURYSTYCZNE_OBIEKTY.turysci_zagraniczni_ogolem ,df_cor)
-
-Metrics::rmse(df_cor$touristic_popularity, m1$fitted.values)
-Metrics::mape(df_cor$touristic_popularity, m1$fitted.values)
-
-full_data %>%
-  select(TURYSTYCZNE_OBIEKTY.turysci_zagraniczni_ogolem, year, city) %>%
-  filter(!is.na(TURYSTYCZNE_OBIEKTY.turysci_zagraniczni_ogolem)) %>%
-  arrange(year) %>%
-  dim()
+save(cleaned_data, file = 'data/100_CleanedData.Rdata')
 
