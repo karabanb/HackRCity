435a639ec8a7f1b96e4626d6f9722bb4bf71022c
jarekopala <jarek.opalaaa@gmail.com>
2019-11-16 16:35:42 +0100

    explain

--- scripts/300_Explain.R
+++ scripts/300_Explain.R
@@ -1,7 +1,10 @@
 
 load('results/201/automl1.RData')
 
-# h2o.loadModel(paste0(getwd(), '/results/201/DeepLearning_grid_1_AutoML_20191116_160212_model_7'))
+popIndexModel <- h2o.loadModel(paste0(getwd(), '/results/201/DeepLearning_grid_1_AutoML_20191116_160212_model_7'))
 
+vi_autoML_h2o <- h2o.varimp(popIndexModel)
 
+h2o.varimp_plot(popIndexModel, num_of_features = 10)
 
+vi_autoML <- variable_importance(explainer_h2o_automl1, n_sample = 10)
